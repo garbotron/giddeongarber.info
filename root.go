c404854089da09ac900dcce9d3a76f784de422f0
Giddeon Garber <giddeon.garber@gmail.com>
2014-10-23 21:58:16 -0700

    Move root to correct path

--- root.go
+++ root.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Init() error {
-	http.Handle("/gg/", http.StripPrefix("/gg/", http.FileServer(http.Dir(os.ExpandEnv("$GOPATH/src/giddeongarber.info")))))
+	http.Handle("/gg/", http.StripPrefix("/gg/", http.FileServer(http.Dir(os.ExpandEnv("$GOPATH/src/github.com/garbotron/giddeongarber.info")))))
 	return nil
 }
