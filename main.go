8e1fbeb585911a593d18f97bf9f4d577ff16c929
Giddeon Garber <giddeon.garber@gmail.com>
2014-10-23 21:33:40 -0700

    Move primary location

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
-	http.Handle("/gg/", http.StripPrefix("/gg/", http.FileServer(http.Dir("/www/giddeongarber.info"))))
+	http.Handle("/gg/", http.StripPrefix("/gg/", http.FileServer(http.Dir(os.ExpandEnv("$GOPATH/src/giddeongarber.info")))))
 	panic(http.ListenAndServe(":80", nil))
 }
